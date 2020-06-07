84e1edd50cc254d019a7f2c30727a33ceae437c8
David Carmona <davidcarmona@gmail.com>
2020-06-07 11:07:39 -0600

    added code for DOT PROMISE and aysnc

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 /// DEPENDENCIES
 const inquirer = require("inquirer");
 const inquirerConfirmValidated = require("inquirer-confirm-validated");
+const employeeOp = require("./lib/Employee");
+const engineerOp = require("./lib/Engineer");
+const internOp = require("./lib/Inter");
+const managerOp = require("./lib/Manager");
 /// NATIVE
 const util = require("util");
 const fs = require("fs");
+const writeFileAsync = util.promisify(fs.writeFile);
 
-/// VARIABLES
-let employeeList = [];
-let managerList = [];
-let engineerList = [];
-let internList = [];
-
-/// FUNCTIONS
+/// FUNCTIONS 14-122 and 184-323 for DOT THEN PROMISE
 const confirmString = async (input) => {
   if (typeof input !== "string" || input.trim("") === "") {
-    return "Expected parameter to be a non-empty string";
+    return `Expected parameter to be a non-empty string`;
   } else {
     return true;
   }
@@ -22,7 +21,7 @@ const confirmString = async (input) => {
 
 const confirmNumber = async (input) => {
   if (typeof input !== "number" || input > 0) {
-    return "Expected parameter to be a number greater than zero";
+    return `Expected parameter to be a number greater than zero`;
   } else {
     return true;
   }
@@ -37,6 +36,7 @@ const confirmRole = async (answer) => {
   }
 };
 
+////////////// ENGINEER PROMPT \\\\\\\\\\\\\
 function engineerPrompt() {
   inquirer.prompt([
     {
@@ -66,6 +66,7 @@ function engineerPrompt() {
   ]);
 }
 
+////////////// INTER PROMPT \\\\\\\\\\\\\
 function internPrompt() {
   inquirer.prompt([
     {
@@ -95,7 +96,31 @@ function internPrompt() {
   ]);
 }
 
-/// BULK CODE
+////// END TERMINAL PROMPT \\\\\\
+function done() {
+  return;
+}
+
+//////////////////// DELETE \\\\\\\\\\\\\\\\\\\\\\\\
+////////// select prompt based of answer \\\\\\\\\\\\
+function setEmployees(answer) {
+  if (answer.role === "Engineer") {
+    engineer(answer.name, answer.id, answer.email, answer.role, answer.gitHub);
+    return true;
+  }
+  if (answer.role === "Intern") {
+    intern(answer.name, answer.id, answer.email, answer.role, answer.school);
+    return true;
+  }
+  if (answer.role === "I don't add anymore.") {
+    writeFileAsync("index.html", employeeList).then(function () {
+      console.log(`Your Employee list was generated!`);
+    });
+    done();
+  }
+}
+
+////// BULK CODE \\\\\\
 async function EmployeeRegistration() {
   try {
     const employee = await inquirer.prompt([
@@ -104,6 +129,12 @@ async function EmployeeRegistration() {
         message: "What is the manager's name?",
         type: "input",
         // validate: confirmString,
+        // validate: (input) => {
+        //   if (input.trim("") !== "string" || input.trim("") === "") {
+        //   return `Expected parameter to be a non-empty string`;
+        //   }
+        //   return true;
+        // },
       },
       {
         name: "id",
@@ -128,13 +159,165 @@ async function EmployeeRegistration() {
         message: "What type of team member are they?",
         type: "list",
         choices: ["Engineer", "Intern"],
-        // validate: confirmRole,
+        validate: confirmRole,
       },
     ]);
 
-    // const engineer =
+    //  SHOULD BE IN OR WITH A CONDITIONAL TO CALL ON OR THE OTHER
+    // const engineer = await engineerPrompt();
+    // const intern = await internPrompt();
+    // const done = await done()
   } catch (err) {
     console.log(`${err} error??`);
   }
 }
-EmployeeRegistration();
+// EmployeeRegistration();
+
+/// VARIABLES \\\
+let managerList = [];
+let engineerList = [];
+let internList = [];
+let employeeList = [...managerList, ...engineerList, ...internList];
+
+////////////// DOT PROMISE \\\\\\\\\\\\\\
+employees();
+function employees() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the manager's name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What is the manager's id",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is the manager's email?",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "What is the manager's number?",
+        name: "number",
+      },
+      {
+        type: "list",
+        message: "What type of team member are they?",
+        name: "role",
+        choices: ["Engineer", "Intern", "I don't add anymore."],
+      },
+    ])
+    /////////// DOT THEN \\\\\\\\\\
+    // inside function switch statement for 3 functions
+    .then(function (answer) {
+      setEmployees(answer);
+    })
+    // .then(function (answer) {
+    //   if (answer.role === "Engineer") {
+    //     engineer(
+    //       answer.name,
+    //       answer.id,
+    //       answer.email,
+    //       answer.role,
+    //       answer.gitHub
+    //     );
+    //     return true;
+    //   }
+    //   if (answer.role === "Intern") {
+    //     intern(
+    //       answer.name,
+    //       answer.id,
+    //       answer.email,
+    //       answer.role,
+    //       answer.school
+    //     );
+    //     return true;
+    //   }
+    //   if (answer.role === "I don't add anymore.") {
+    //     writeFileAsync("index.html", employeeList).then(function () {
+    //       console.log(`Your Employee list was generated!`);
+    //     });
+    //     done();
+    //   }
+    // })
+    .catch(function (err) {
+      return `Error!!`;
+    });
+}
+
+////////////// INTER PROMPT \\\\\\\\\\\\\
+function intern(name, id, email, role, school) {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What's your intern's name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What's your intern's id",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What's your intern's email?",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "What is your intern's school?",
+        name: "school",
+      },
+      {
+        type: "list",
+        message: "Add another member?",
+        name: "role",
+        choices: ["Engineer", "Intern", "I don't add anymore."],
+      },
+    ])
+    .then((interObj) => {
+      const intern = new Intern(name, id, email, role, internObj.school);
+    });
+}
+
+////////////// ENGINEER PROMPT \\\\\\\\\\\\\
+function engineer(name, id, email, role, gitHub) {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What's your engineer's name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What's your engineer's id",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What's your engineer's email?",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "What's your engineer's GitHub username?",
+        name: "github",
+      },
+      {
+        type: "list",
+        message: "Add another member?",
+        name: "role",
+        choices: ["Engineer", "Intern", "I don't add anymore."],
+      },
+    ])
+    .then((engineerObj) => {
+      const engineer = new Engineer(name, id, email, role, engineerObj.gitHub);
+      console.log(engineer);
+    });
+}
