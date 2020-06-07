cbad335db574cf1311ab1c93de9bfea39ba73cc4
David Carmona <davidcarmona@gmail.com>
2020-06-06 18:08:39 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+/// DEPENDENCIES
+const inquirer = require("inquirer");
+const util = require("util");
+const fs = require("fs");
+
+/// FUNCTIONS
+
+/// BULK CODE
+
+/// VARIABLES
+
+let employeeList = [];
+let managerList = [];
+let engineerList = [];
+let internList = [];
